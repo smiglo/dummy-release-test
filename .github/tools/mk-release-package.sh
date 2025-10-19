93e1c05f9df4c7ab2233e2e3d8b6e4151c326006
Tomasz <tomek.blaszczak@gmail.com>
2025-10-19 22:33:53 +0200

    fix: create asset with sources

--- .github/tools/mk-release-package.sh
+++ .github/tools/mk-release-package.sh
@@ -2,15 +2,14 @@
 
 set -e
 
-set -xv
 version="${1:-0.1.0-unknown}"
 cmake -S . -B build -DPROJECT_VERSION=$version
 cmake --build build
 dist_name="hello_world-$version"
 dist_path="build/$dist_name"
 mkdir -p "$dist_path"
+rm -f build/hello_world-*.tar.gz
 git checkout-index -a -f --prefix="$dist_path/"
 sed -i -e '/# <VERSION_BLOCK>/,/# <\/VERSION_BLOCK>/cset(PROJECT_VERSION \"'"$version"'\")' "$dist_path/CMakeLists.txt"
 tar -czf "build/$dist_name.tar.gz" -C "build" "$dist_name"
-ls -al "build/$dist_name.tar.gz"
 
